0bf7997ed0b7017e751b6486d35f2b278160f22f
Hana Pham <hagakui90@gmail.com>
2024-10-15 10:28:46 +0700

    Run on Multiple Browsers: Update Account_01_Register, RunNopCommerceTestcases

--- testcases/com/nopcommerce/account/Account_01_Register.java
+++ testcases/com/nopcommerce/account/Account_01_Register.java
@@ -4,10 +4,13 @@ import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import commons.BasePage;
@@ -19,22 +22,27 @@ public class Account_01_Register extends BasePage {
 	private WebDriver driver;
 	private String emailAddress;
 	private String projectPath = System.getProperty("user.dir");
-	private String osName = System.getProperty("os.name");
-	
+
 	private HomePageObject homePage;
 	private RegisterPageObject registerPage;
 	private CustomerPageObject customerPage;
 
+	@Parameters("browser")
 	@BeforeClass
-	public void beforeClass() {
-		if (osName.contains("Mac")) {
-			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver");
-		} else {
+	public void beforeClass(String browserName) {
+		if (browserName.equalsIgnoreCase("firefox")) {
 			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
-
+			driver = new FirefoxDriver();
+		} else if (browserName.equalsIgnoreCase("chrome")) {
+			System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
+			driver = new ChromeDriver();
+		} else if (browserName.equalsIgnoreCase("edge")) {
+			System.setProperty("webdriver.edge.driver", projectPath + "\\browserDrivers\\msedgedriver.exe");
+			driver = new EdgeDriver();
+		} else {
+			throw new RuntimeException("Browser name is not valid");
 		}
 
-		driver = new FirefoxDriver();
 		emailAddress = "afc" + generateFakeNumber() + "@mail.vn";
 
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
@@ -46,9 +54,9 @@ public class Account_01_Register extends BasePage {
 	@Test
 	public void Register_01_Empty() {
 		homePage.clickToRegisterLink();
-		
+
 		registerPage = new RegisterPageObject(driver);
-		
+
 		registerPage.clickToRegisterButton();
 		Assert.assertEquals(registerPage.getFirstNameErrorTextMessage(), "account.fields.firstname.required");
 		Assert.assertEquals(registerPage.getLastNameErrorTextMessage(), "account.fields.lastname.required");
@@ -61,16 +69,16 @@ public class Account_01_Register extends BasePage {
 	public void Register_02_Invalid_Email() {
 		registerPage.clickToNopCommerceLogo();
 		homePage = new HomePageObject(driver);
-		
+
 		homePage.clickToRegisterLink();
 		registerPage = new RegisterPageObject(driver);
-		
+
 		registerPage.enterToFirstNameTextbox("Hana");
 		registerPage.enterToLastNameTextbox("Hasta");
 		registerPage.enterToEmailTextbox("21334@vn");
 		registerPage.enterToPasswordTextbox("123456");
 		registerPage.enterToConfirmPasswordTextbox("123456");
-		
+
 		registerPage.clickToRegisterButton();
 
 		Assert.assertEquals(registerPage.getEmailErrorTextMessage(), "common.wrongemail");
@@ -81,7 +89,7 @@ public class Account_01_Register extends BasePage {
 	public void Register_03_Successful() {
 		registerPage.clickToNopCommerceLogo();
 		homePage = new HomePageObject(driver);
-		
+
 		homePage.clickToRegisterLink();
 		registerPage = new RegisterPageObject(driver);
 
@@ -92,9 +100,9 @@ public class Account_01_Register extends BasePage {
 		registerPage.enterToConfirmPasswordTextbox("123456");
 
 		registerPage.clickToRegisterButton();
-		Assert.assertEquals(registerPage.getCompletedRegisterTextMessage(), "account.register.result.standard");		
+		Assert.assertEquals(registerPage.getCompletedRegisterTextMessage(), "account.register.result.standard");
 		registerPage.clickToMyAccountLink();
-		
+
 		customerPage = new CustomerPageObject(driver);
 		Assert.assertEquals(customerPage.getFirstNameTextboxAttributeValue(), "Hana");
 		Assert.assertEquals(customerPage.getLastNameTextboxAttributeValue(), "Hasta");
@@ -116,16 +124,16 @@ public class Account_01_Register extends BasePage {
 		registerPage.enterToConfirmPasswordTextbox("123456");
 
 		registerPage.clickToRegisterButton();
-		
+
 		Assert.assertEquals(registerPage.getExistingEmailErrorTextMessage(), "account.register.errors.emailalreadyexists");
-		
+
 	}
 
 	@Test
 	public void Register_05_Password_Less_Than_6_Characters() {
 		registerPage.clickToNopCommerceLogo();
 		homePage = new HomePageObject(driver);
-		
+
 		homePage.clickToRegisterLink();
 		registerPage = new RegisterPageObject(driver);
 
@@ -144,7 +152,7 @@ public class Account_01_Register extends BasePage {
 	public void Register_06_Not_Matching_Password() {
 		registerPage.clickToNopCommerceLogo();
 		homePage = new HomePageObject(driver);
-		
+
 		homePage.clickToRegisterLink();
 		registerPage = new RegisterPageObject(driver);
 
