36e20a15c3d7c1736d66378a566fd21108d224a6
Hana Pham <hagakui90@gmail.com>
2024-10-16 13:00:58 +0700

    Apply WebDriverManager: BaseTest

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -6,26 +6,32 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+/*import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;*/
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseTest {
-	private String projectPath = System.getProperty("user.dir");
+//	private String projectPath = System.getProperty("user.dir");
 	private WebDriver driver;
 
 	protected WebDriver getBrowserDriver(String browserName) {
 		BrowserList browser = BrowserList.valueOf(browserName.toUpperCase());
 		
 		if (browser == BrowserList.FIREFOX) {
-			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
-			driver = new FirefoxDriver();
+			//System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+			driver = WebDriverManager.firefoxdriver().create();
+			//driver = new FirefoxDriver();
 		} else if (browser == BrowserList.CHROME) {
-			System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
-			driver = new ChromeDriver();
+			//System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
+			//driver = new ChromeDriver();
+			driver = WebDriverManager.chromedriver().create();
 		} else if (browser == BrowserList.EDGE) {
-			System.setProperty("webdriver.edge.driver", projectPath + "\\browserDrivers\\msedgedriver.exe");
-			driver = new EdgeDriver();
+			//System.setProperty("webdriver.edge.driver", projectPath + "\\browserDrivers\\msedgedriver.exe");
+			//driver = new EdgeDriver();
+			driver = WebDriverManager.edgedriver().create();
+
 		} else {
 			throw new RuntimeException("Browser name is not valid");
 		}
