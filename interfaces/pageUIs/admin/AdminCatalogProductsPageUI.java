1e1f3428204e0e6d241301545cb2116c3033ed8c
Hana Pham <hagakui90@gmail.com>
2024-11-18 16:26:16 +0700

    Create testcase Sort_05_Created_On(TBD)

---
+++ interfaces/pageUIs/admin/AdminCatalogProductsPageUI.java
@@ -0,0 +1,9 @@
+package pageUIs.admin;
+
+public class AdminCatalogProductsPageUI {
+	public static final String SEARCH_CATEGORY_TEXTBOX = "css=select#SearchCategoryId";
+	public static final String SEARCH_BUTTON = "CSS=button#search-products";
+	public static final String ALL_PAGES_LINK = "XPATH=//li[contains(@class,'paginate_button')]//a[not(contains(text(),'admin.dt.paginate'))]";
+	public static final String COLUMN_INDEX_BY_COLUMN_NAME = "XPATH=//div[text()='%s']/parent::th//preceding-sibling::th";
+	public static final String ALL_VALUES_COLUMN_INDEX = "XPATH=//tr/td[%s]";
+}
--- testcases/com/nopcommerce/view/View_02_Sort_Display_Paging.java
+++ testcases/com/nopcommerce/view/View_02_Sort_Display_Paging.java
@@ -8,7 +8,10 @@ import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import commons.BaseTest;
+import commons.GlobalConstants;
 import commons.PageGeneratorManager;
+import pageObjects.admin.AdminDashboardPageObject;
+import pageObjects.admin.AdminLoginPageObject;
 import pageObjects.guest.BooksSubPageObject;
 import pageObjects.guest.ComputersCategoryPageObject;
 import pageObjects.guest.NotebooksSubPageObject;
@@ -20,6 +23,10 @@ public class View_02_Sort_Display_Paging extends BaseTest {
 	private ComputersCategoryPageObject computersPage;
 	private NotebooksSubPageObject notebooksSubPage;
 	private BooksSubPageObject booksSubPage;
+	private AdminDashboardPageObject adminDashboardPage;
+	private AdminLoginPageObject adminLoginPage;
+	private String userUrl = GlobalConstants.DEV_USER_URL;
+	private String adminUrl = GlobalConstants.DEV_ADMIN_URL;
 
 	@Parameters("browser")
 	@BeforeClass
@@ -35,25 +42,33 @@ public class View_02_Sort_Display_Paging extends BaseTest {
 		notebooksSubPage = PageGeneratorManager.getNotebooksSubPage(driver);
 		Assert.assertEquals(notebooksSubPage.getPageTitle(), "Notebooks");
 	}
-
+	@Test
 	public void Sort_01_Name_A_To_Z() {
 		Assert.assertTrue(notebooksSubPage.verifySortNameAscending());
 	}
-
+	@Test
 	public void Sort_02_Name_Z_To_A() {
 		Assert.assertTrue(notebooksSubPage.verifySortNameDescending());
 	}
 
-	
+	@Test
 	public void Sort_03_Price_Low_To_High() {
 		Assert.assertTrue(notebooksSubPage.verifySortPriceAscending());
 	}
 
-	
+	@Test
 	public void Sort_04_Price_High_To_Low() {
 		Assert.assertTrue(notebooksSubPage.verifySortPriceDescending());
 	}
 
+	@Test
+	public void Sort_05_Created_On() {
+		notebooksSubPage.openPageUrl(driver, adminUrl);
+		Assert.assertTrue(homePage.isPageLoadedSuccess(driver));
+		adminLoginPage = PageGeneratorManager.getAdminLoginPage(driver);
+		adminDashboardPage = adminLoginPage.enterToLoginForm(GlobalConstants.DEV_ADMIN_USERNAME, GlobalConstants.DEV_ADMIN_PASSWORD);
+	} 
+	
 	@Test
 	public void Display_05_3_Products_Per_Page() {
 		notebooksSubPage.openCategoriesSideBarPage("Books", "");
